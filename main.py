81d9409590c6f4094b0f4dbafc432e66b0baf825
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-06-12 23:58:25 +0530

    added the location cordinates

--- main.py
+++ main.py
@@ -2,15 +2,26 @@ import phonenumbers
 import os
 from dotenv import load_dotenv
 from phonenumbers import geocoder, carrier
+from opencage.geocoder import OpenCageGeocode
 
 load_dotenv()
 
 number = os.getenv('NUMBER')
+key = os.getenv("GEOCODEAPI")
+
 parsed_number = phonenumbers.parse(number)
 location = geocoder.description_for_number(parsed_number, 'en')
 
 carrier_name = carrier.name_for_number(parsed_number, "en")
 
+geocoder_info = OpenCageGeocode(key)
+query = str(location)
+result = geocoder_info.geocode(query)
+
+lat = result[0]['geometry']['lat']
+lng = result[0]['geometry']['lng']
+
 print(location)
 print(carrier_name)
-
+# print(result)
+print(lat, lng)
