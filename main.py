162a9415fb67a43d8a1af8d64793ea9a25a98b65
Dope-Otaku <souvik.halder.btech2020@sitpune.edu.in>
2024-06-18 23:17:05 +0530

    added some random commnets and again trying to be constant on github and dsa

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import phonenumbers
 import os
+import folium
 from dotenv import load_dotenv
 from phonenumbers import geocoder, carrier
 from opencage.geocoder import OpenCageGeocode
@@ -20,6 +21,11 @@ result = geocoder_info.geocode(query)
 
 lat = result[0]['geometry']['lat']
 lng = result[0]['geometry']['lng']
+
+
+myMap = folium.Map(location=[lat, lng], zoom_start=9)
+folium.Marker([lat, lng], popup=location).add_to(myMap)
+myMap.save("myLocation.html") #file name in which your location will be saved
 
 print(location)
 print(carrier_name)
